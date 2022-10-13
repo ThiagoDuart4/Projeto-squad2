11142dd77c81e856ad8fa766509a879f9d3d8a09
Thiago Duarte <106554492+ThiagoDuart4@users.noreply.github.com>
2022-10-13 11:48:37 -0300

    Add files via upload
    adicionando implementaçoes globais,css e javascript, adicionando comentarios

---
+++ js/global.js
@@ -0,0 +1,9 @@
+// SIDE BAR NAV header
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
