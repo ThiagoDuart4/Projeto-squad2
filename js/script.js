d5be38704046254e0215eaea95e8acb66ed3fc8f
Thiago Duarte <106554492+ThiagoDuart4@users.noreply.github.com>
2022-10-26 10:55:55 -0300

    Concertando commits

--- js/script.js
+++ js/script.js
@@ -1,19 +1,11 @@
 /* aqui é o código para dar alert quando a pessoa preencher o formulário */ 
 
-function validate ()
-{
-var username=document.getElementById("username").nodeValue;
-var password=document.getElementById("password").nodeValue;
-
-if(username=="admin"&& password=="user")
-
-{
-     alert("login succesfuly");
-     return false;
-}
-else
-{
-     alert("login efetuado");
-}
-
+function abrirAlerta(){
+     swal({
+          title:"Olá",
+          text:"Seja bem vindo a HC Student",
+          icon:"success",
+          button: "Aww yiss!",
+          time: 50.000,
+     });
 }
